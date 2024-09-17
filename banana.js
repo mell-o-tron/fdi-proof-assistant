a069ce3d3e143a4993d7aaa86b5a827f184663b6
mell-o-tron <l.pace4@studenti.unipi.it>
2024-09-17 13:44:39 +0200

    added text injector and overridden evaluation controls

--- banana.js
+++ banana.js
@@ -1,20 +1,29 @@
+import { JsCoq } from './node_modules/jscoq/jscoq.js';
+
+var jscoq_ids  = ['coq-code'];
+var jscoq_opts = {
+prelude:   true,
+implicit_libs: true,
+editor:{ mode: { 'company-coq': true }, keyMap: 'default' },
+init_pkgs: ['init'],
+all_pkgs:  ['coq']
+};
+
+let manager_promise = JsCoq.start(jscoq_ids, jscoq_opts);
 
 
+manager_promise.then(function(manager) {
+  
+  manager.when_ready.then(function (result){
+    console.log("READYYYYYYYYYYYYYYYYYYYYYYYY")
+    let snippet = manager.provider.getSnippets()[0]
+    console.log(snippet)
+    snippet.injectText("Let x := 5.\nPrint x.\n")
+    
+    manager.provider.focus();
+    manager.goNext(true)
+    manager.goNext(true)
+  });
 
-// Function to be executed after delay
-function delayedFunction() {
-    console.log('This message appears after 2 seconds.');
-    const xpath = '/html/body/div/div[1]/div/div/div[1]/textarea';
-    // Evaluate the XPath expression
-    const xpathResult = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
 
-    // Get the element from the result
-    const textareaElement = xpathResult.singleNodeValue;
-
-    console.log(textareaElement)
-
-    textareaElement.textContent = "PIPO"
-}
-
-// Run the function after 2000 milliseconds (2 seconds)
-setTimeout(delayedFunction, 2000);
+});
