012b9bad05292b52090d7d0a7365aba17e8e4090
dariof4 <dazedtank@gmail.com>
2024-11-07 20:43:12 +0100

    Make texifier work with multiple if-then-else

--- src/texifier.js
+++ src/texifier.js
@@ -41,19 +41,25 @@ class TeXifier {
     texify(text) {
         let res = text;
         
-        let m = res.match(/if\s+(.+?)\s+then\s+(.+?)\s+else/);
-        
-        if(m && !m[1].includes("if") && !m[1].includes("then") && !m[1].includes("else") && !m[2].includes("if") && !m[2].includes("then") && !m[2].includes("else")) {
-            let beginning_index = res.indexOf("if");
-            let beginning_of_end = res.indexOf("else");
-            let end_index = end_or_first_unmatched_rpar(res, beginning_of_end);
+        let mg = res.match(/if\s+(.+?)\s+then\s+(.+?)\s+else/g);
+        let findex = 0;
+        if (mg) {
+            for (let i = 0; i<mg.length; i++) {
+                let m = mg[i].match(/if\s+(.+?)\s+then\s+(.+?)\s+else/);
+                if(m && !m[1].includes("if") && !m[1].includes("then") && !m[1].includes("else") && !m[2].includes("if") && !m[2].includes("then") && !m[2].includes("else")) {
+                    let beginning_index = res.indexOf("if", findex);
+                    let beginning_of_end = res.indexOf("else", findex);
+                    let end_index = end_or_first_unmatched_rpar(res, beginning_of_end);
             
-            let else_branch = res.slice(beginning_of_end, end_index).replace("else", "");
+                    let else_branch = res.slice(beginning_of_end, end_index).replace("else", "");
             
-            // TODO does not work for nested ifs
-            // TODO does not work for more than one if in an expression: could call recursively but would break comma-placeholder - maybe do separate function to be called recursively before texify.
+                    // TODO does not work for nested ifs
+                    // TODO does not work for more than one if in an expression: could call recursively but would break comma-placeholder - maybe do separate function to be called recursively before texify.
             
-            res = res.replace(res.slice(beginning_index, end_index), `\\begin{cases} ${m[2]} & \\text{ if }${m[1]}\\\\${else_branch} & \\text{ otherwise}\\end{cases}`)
+                    res = res.replace(res.slice(beginning_index, end_index), `\\begin{cases} ${m[2]} & \\text{ if }${m[1]}\\\\${else_branch} & \\text{ otherwise}\\end{cases}`)          
+                    findex = end_index;
+                }
+            }
         }
         
         for (let r of this.replacements) {
