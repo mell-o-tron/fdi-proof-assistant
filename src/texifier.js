26990c17ffab10bb11672680a1dd3cabc41f23bc
mell-o-tron <l.pace4@studenti.unipi.it>
2024-11-27 20:39:54 +0100

    Fixed if visualization bug

--- src/texifier.js
+++ src/texifier.js
@@ -69,53 +69,24 @@ class TeXifier {
         ];
     }
 
-    replace_ifs (text) {
-        let res = text;
-        
-        let m = res.match(/if\s+(.+?)\s+then\s+(.+?)\s+else/);
-        
-        if(m && !m[1].includes("if") && !m[1].includes("then") && !m[1].includes("else") && !m[2].includes("if") && !m[2].includes("then") && !m[2].includes("else")) {
-            let beginning_index = res.indexOf("if");
-            let beginning_of_end = res.indexOf("else");
-            let end_index = end_or_first_unmatched_rpar(res, beginning_of_end);
-            
-            let else_branch = res.slice(beginning_of_end, end_index).replace("else", "");
-            
-            // TODO does not work for nested ifs
-            // TODO does not work for more than one if in an expression: could call recursively but would break comma-placeholder - maybe do separate function to be called recursively before texify.
-            
-            res = res.replace(res.slice(beginning_index, end_index), `\\begin{cases} ${m[2]} & \\text{ if }${m[1]}\\\\${else_branch} & \\text{ otherwise}\\end{cases}`)
-            
-        }
-        
-        res = res.replace(/\\bigg\s*\(\s*\\begin\{cases\}/, "(\\begin{cases}")
-        res = res.replace(/\\end\{cases\}\s*\\bigg\s*\)/, "\\end{cases})")
-        
-        res = res.replace(/\(\s*\\begin\{cases\}/, "\\bigg(\\begin{cases}")
-        res = res.replace(/\\end\{cases\}\s*\)/, "\\end{cases}\\bigg)")
-        
-        return res;
-        
-    }
     
     texify_common(text){
-        let res = this.replace_ifs(text);
+       
+        // visualizing ifs
+        let res = text;
+        
+        res = res.replaceAll("#cases", "\\begin{cases} ")
+                 .replaceAll("#if", "& \\text{if }")
+                 .replaceAll("#otherwise", "\\\\")
+                 .replaceAll("#endcases", "& \\text{otherwise}\\end{cases}");
+                 
         
 
-        // code for visualizing exponents
+        // visualizing exponents
 
         res = res.replaceAll("#^", "^{").replaceAll("^#", "}");
 
-        // for (let x of res.matchAll(/\^/g)) {
-        //     if (res.substring(x.index+1).trim().startsWith("(")) {
-        //         let ind = end_or_first_unmatched_rpar(res,x.index)
-        //         res = res.substring(0, x.index+1) + res.substring(x.index+1, ind).replace("(","{") + "}" + res.substring(ind+1)
-        //     }
-        //     else {
-        //         let after = res.substring(x.index+1).trim().match(/(?:^(.+?)\s+(.*)|^.+)/s)
-        //         res = res.substring(0,x.index+1) + "{" + (after[1] ? after[1] : after[0]) + "}" + (after[2] ? after[2] : "")
-        //     }
-        // }
+        
         
         for (let r of this.replacements) {
             res = res.replaceAll(r[0], r[1]);
